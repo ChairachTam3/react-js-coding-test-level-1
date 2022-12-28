7a06219b100a121a540dfac013c7fd729a3e3ba7
ChairachTam3 <chairachtam3@gmail.com>
2022-12-28 15:49:26 +0700

    Update form validation

--- src/Home.js
+++ src/Home.js
@@ -6,24 +6,35 @@ function Home() {
   const [text, setText] = useState("");
   const [isReady, setIsReady] = useState(false);
 
+  const handleChange = (e) => {
+    setText(e.target.value);
+    if (e.target.value === "Ready!") {
+      setIsReady(true);
+    } else {
+      setIsReady(false);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
-        <b>
+        <NavLink to="/PokeDex">
+          <img
+            hidden={!isReady}
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            className="App-logo"
+            alt="logo"
+            style={{ padding: "10px" }}
+          />
+        </NavLink>
+        {/* <b>
           Requirement: Try to show the hidden image and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
           red text away when "Ready!" is in the textbox.
-        </b>
+        </b> */}
         <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        <input type="text" name="name" value={text} onChange={handleChange} />
+        {!isReady && <span style={{ color: "red" }}>I am not ready yet!</span>}
       </header>
     </div>
   );
